376432e94bfac6de42af91508da0fb4807b2e3d9
Dipu <hdipu2646@diu.edu.bd>
2021-06-06 00:37:51 +0600

    Change Title

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="MD HASANUZZAMAN DIPU 202000000"/>
+    <HelloWorld msg="MD HASANUZZAMAN DIPU "/>
   </div>
 </template>
 
