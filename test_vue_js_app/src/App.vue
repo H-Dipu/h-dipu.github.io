b5a2fe349409d89d88e6007d31e8646b2f5d67e6
Dipu <hdipu2646@diu.edu.bd>
2021-06-06 16:16:58 +0600

    Bootstrap add

--- test_vue_js_app/src/App.vue
+++ test_vue_js_app/src/App.vue
@@ -7,7 +7,35 @@
              :lastName="lName"
              :age="age"
              @updateFirstname="fName=$event"
-    /> <cFooter/>
+    />
+    <cListHelper>
+      <ul slot="list">
+        <li v-for="lsHelper in listHelper" :key="lsHelper">{{lsHelper}} </li>
+      </ul>
+       <p slot="para"> This is paragraphs slot </p>
+
+      <table :slot="loader" class="table table-striped table-bordered">
+        <thead>
+        <tr>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Role</th>
+          <th>Age</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="(user, index) in users" :key="index">
+          <td>{{user.firstName}} {{user.lastName}}</td>
+          <td>{{user.email}}</td>
+          <td>{{user.role}}</td>
+          <td>{{user.age}}</td>
+        </tr>
+        </tbody>
+      </table>
+
+
+    </cListHelper>
+    <cFooter/>
 
   </div>
 </template>
@@ -15,21 +43,39 @@
 <script>
 import cHeader from './components/Header_Footer/Header';
 import cFooter from './components/Header_Footer/Footer';
+import cListHelper  from './components/ListHelper';
 export default {
   data(){
     return{
      Name:'Dipu',
      students:['Dipu','Al-amin','Rasel','Parvez','Hasan'],
+      users: [
+        { firstName: 'Frank', lastName: 'Murphy', email: 'frank.murphy@test.com', role: 'User',age:20 },
+        { firstName: 'Vic', lastName: 'Reynolds', email: 'vic.reynolds@test.com', role: 'Admin' },
+        { firstName: 'Gina', lastName: 'Jabowski', email: 'gina.jabowski@test.com', role: 'Admin' },
+        { firstName: 'Jessi', lastName: 'Glaser', email: 'jessi.glaser@test.com', role: 'User' },
+        { firstName: 'Jay', lastName: 'Bilzerian', email: 'jay.bilzerian@test.com', role: 'User' }
+      ],
 
       fName:'Md Hasanuzzaman',
       lName:'Dipu',
-      age:28
+      age:28,
+
+      listHelper:['Html','CSS','Bootstraps','JavaScript','Laravel','Vue Js'],
+      loader:''
+
 
     }
   },
   components:{
     cHeader,
-    cFooter
+    cFooter,
+    cListHelper
+  },
+  created() {
+    setTimeout(()=>{
+      this.loader='other'
+    },3000)
   }
 
 }
---
+++ test_vue_js_app/src/components/ListHelper.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+  Hello I am List Helper slot
+  <slot name="list"></slot>
+  <slot name="para"></slot>
+  <slot name="other"> Loading data plz wait</slot>
+</div>
+</template>
+
+<script>
+export default {
+    data(){
+      return{
+
+      }
+    }
+ }
+</script>
+
+<style>
+ul li{
+  color: brown;
+}
+p {
+  color: green;
+}
+
+</style>
