031e332f7397810ecb8cfc164a35889cb3d02313
Dipu <hdipu2646@diu.edu.bd>
2021-06-07 04:15:55 +0600

    mixins

---
+++ test_vue_js_app/src/components/Client.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <p>Client Data</p>
+    <p>{{text}}</p>
+    <ul>
+      <li v-for="car in cars" :key="car"> {{car}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import  {carMixin} from "./mixins";
+
+export  default {
+   data(){
+     return{
+       // text:" Hello World",
+
+     }
+   },
+  mixins:[carMixin]
+
+ }
+</script>
+
+<style>
+
+</style>
---
+++ test_vue_js_app/src/components/User.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <p>User Data</p>
+    <p>{{text}}</p>
+    <ol>
+      <li v-for="car in cars" :key="car"> {{car}}</li>
+    </ol>
+  </div>
+</template>
+
+<script>
+import  {carMixin} from "./mixins";
+
+export  default {
+  data(){
+    return{
+      // text:" Hello World",
+
+    }
+  },
+  mixins:[carMixin]
+
+}
+</script>
+
+<style>
+
+</style>
