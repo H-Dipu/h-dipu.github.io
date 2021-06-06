425a1860008bd6c5227a39f4f27a965f557ab813
Dipu <hdipu2646@diu.edu.bd>
2021-06-06 07:12:43 +0600

    dasdsads

---
+++ test_vue_js_app/src/components/Header_Footer/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+  <div id="app">
+    <p> Footer sddsfdsf </p>
+  </div>
+</template>
---
+++ test_vue_js_app/src/components/Header_Footer/Header.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <p> Header ddd {{props_myName}}</p>
+    <ol>
+    <li v-for="stu in props_student" :key="stu">
+      {{stu}}
+    </li></ol>
+
+    <p>First Name: {{firstName}}</p>
+    <p> Last Name: {{lastName}}</p>
+    <p> Age: {{age}}</p>
+    <button @click="updateName()"> Change Name</button>
+
+  </div>
+
+
+</template>
+
+<script>
+export default {
+ // props:['props_myName','props_student'],
+  props:{
+    props_myName:{
+          type:String,
+          required:true
+        },
+    props_student:{
+          type:Array,
+          required: true
+    },
+    firstName: {
+      type:String,
+      required:true
+    },
+    lastName: String,
+    age:Number
+  },
+  methods:{
+    updateName(){
+        // this.firstName=" Al-amin";
+         this.$emit('updateFirstname','Al-amin');
+    }
+  }
+}
+</script>
